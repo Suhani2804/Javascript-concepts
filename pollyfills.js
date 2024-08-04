ce1a3b0e84817b5c55d916ec75f05b0406572a09
Suhani2804 <suhanigupta28@gmail.com>
2024-08-04 10:30:33 +0530

    commit

---
+++ pollyfills.js
@@ -0,0 +1,13 @@
+// pollyfills are a piece of a js code which is used to provide modern functionalities to the old browsers which do not actually support it
+
+//pollyfill for map
+//myMap is the name of our own map function
+function callback(number)
+{
+    return number**2;
+}
+
+Array.prototype.myMap=function(callback){
+    let temparr=[];
+    for(let i=0)
+};
